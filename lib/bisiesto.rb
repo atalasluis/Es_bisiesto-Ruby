ce15d01ea0a5a1c7c0ba51f53abd4a3af9d0b21f
LuisSalazar <atalasturey@gmail.com>
2021-04-27 02:22:30 -0400

    Funcion completa

--- lib/bisiesto.rb
+++ lib/bisiesto.rb
@@ -1,3 +1,7 @@
 def es_bisiesto(num)
-    true
+    if(num % 4 == 0 && num % 100 != 0 || num % 400 == 0)
+        true
+    else
+        false
+    end
 end
--- spec/es_bisiesto_spec.rb
+++ spec/es_bisiesto_spec.rb
@@ -1,5 +1,15 @@
 require './lib/bisiesto.rb'
 RSpec.describe "bisiesto" do
-    it "" do
+    it "deberia devolver true para el año 4" do
+        expect(es_bisiesto(4)).to eq(true)
+    end
+    it "deberia devolver false para el año 5" do
+        expect(es_bisiesto(5)).to eq(false)
+    end
+    it "deberia devolver true para el año 2400" do
+        expect(es_bisiesto(2400)).to eq(true)
+    end
+    it "deberia devolver false para el año 1900" do
+        expect(es_bisiesto(1900)).to eq(false)
     end
 end
